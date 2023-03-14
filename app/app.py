2d7ed26da9a78fe6e3f7efba00cc9ad29930e444
Samu Reinikainen <samu.reinikainen@gmail.com>
2023-03-14 21:19:41 +0200

    added CORS headers

--- app/app.py
+++ app/app.py
@@ -8,6 +8,26 @@ from app.users import auth_backend, current_active_user, fastapi_users, current_
 
 app = FastAPI()
 
+#CORS
+from fastapi.middleware.cors import CORSMiddleware
+""" origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:8000",
+] """
+#allow all origins for testing
+origins = ['*']
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
 app.include_router(
     fastapi_users.get_auth_router(auth_backend), prefix="/auth/jwt", tags=["auth"]
 )
