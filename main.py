5539900682a0789f01a7de575bd96601a22fc7c9
Samu Reinikainen <samu.reinikainen@gmail.com>
2023-03-15 22:24:42 +0200

    added uvicorn auto reload

--- main.py
+++ main.py
@@ -9,5 +9,5 @@
 # run blocks of code only if our program is the main program executed
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("app.app:app", host="0.0.0.0", log_level="info")
+    uvicorn.run("app.app:app", host="0.0.0.0", log_level="info", reload="auto")
     #uvicorn.run("main:app")
